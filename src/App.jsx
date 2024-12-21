617546e3d077860babd398dc8bcd65aa3aadc106
Rana <reduanulislam92665@gmail.com>
2024-12-21 20:14:41 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <>
+    
+      <h1 className='text-center text-red-500'>Hello world </h1>
+     
+    </>
+  )
+}
+
+export default App
