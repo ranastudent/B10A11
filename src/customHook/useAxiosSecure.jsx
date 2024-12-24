1cd676110d45e1d4bd64fa54c151d8fddd2755bc
Rana <reduanulislam92665@gmail.com>
2024-12-25 00:06:26 +0600

    statuse 401 & 403 error must be logout and navigate login page is apply

--- src/customHook/useAxiosSecure.jsx
+++ src/customHook/useAxiosSecure.jsx
@@ -1,10 +1,33 @@
 import axios from "axios";
+import { useEffect } from "react";
+import useAuth from "./useAuth";
+import { useNavigate } from "react-router-dom";
 
 const axiosInstance = axios.create({
       baseURL: 'http://localhost:5000/',
       withCredentials: true
 })
 const useAxiosSecure = () => {
+
+      const { logout } = useAuth()
+      const navigate = useNavigate()
+
+      useEffect(()=>{
+            axiosInstance.interceptors.response.use(response =>{
+                  return response
+            },error =>{
+                  if (error.status === 401 || error.status === 403) {
+                        logout()
+                        .then(()=>{
+                              console.log('logged out user')
+                              navigate('/login')
+                        })
+                        .catch(error => console.log(error))
+                  }
+                  return Promise.reject(error)
+            })
+      },[])
+
       return axiosInstance
 };
 
